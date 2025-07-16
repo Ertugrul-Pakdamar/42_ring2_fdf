25f4ddcb06c7a6c7fd791ac02ea2eaf34ae7171b
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-07-16 18:52:33 +0300

    fix: some leaks fixed;

--- src/ft_free_funcs.c
+++ src/ft_free_funcs.c
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 19:19:16 by epakdama          #+#    #+#             */
-/*   Updated: 2025/07/12 23:30:56 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/07/16 18:47:02 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,15 @@ void	ft_free_3d_matrices(void ***arr)
 
 int	ft_exit_prog(t_vars *vars)
 {
-	mlx_destroy_image(vars->mlx, vars->img);
-	mlx_destroy_window(vars->mlx, vars->win);
-	mlx_destroy_display(vars->mlx);
+	if (vars->img)
+		mlx_destroy_image(vars->mlx, vars->img);
+	if (vars->win)
+		mlx_destroy_window(vars->mlx, vars->win);
+	if (vars->mlx)
+	{
+		mlx_destroy_display(vars->mlx);
+		free(vars->mlx);
+	}
 	ft_free_array((void **)vars->map_2d);
 	ft_free_array((void **)vars->map_3d);
 	free(vars->map_size);
